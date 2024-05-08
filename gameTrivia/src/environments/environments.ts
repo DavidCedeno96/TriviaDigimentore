6ec4b7c9f4a0bd0b4cbeb6481af79d5c0284c7cf
DavidCedeno96 <david.cedeno96@hotmail.com>
2024-05-08 18:29:01 -0500

    en el login esta sin el registrarse
    y cambio de dominio de la api REST

--- gameTrivia/src/environments/environments.ts
+++ gameTrivia/src/environments/environments.ts
@@ -1,5 +1,5 @@
 //export const environment = { URL: 'https://www.triviacmi.somee.com' };
-//export const environment = { URL: 'https://apichallengerdgt.mis-apis.com' };
-export const environment = { URL: 'http://192.168.100.91:8085' };
+export const environment = { URL: 'https://apichallengerdgt.retmeup.com' };
+//export const environment = { URL: 'http://192.168.100.91:8085' };
 
 export const encryptionKey = 'MiClaveSecreta23ParaLaTrivia_Digimentore';
